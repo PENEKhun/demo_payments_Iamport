198b1bbc725c09a47b7f8dfccb480ee17691aa4c
PENEKhun <penekhun@gmail.com>
2022-08-02 14:33:06 +0900

    initial Commit

---
+++ src/main/java/com/example/spring_kginicis_pgtest/SpringKgInicisPGtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_kginicis_pgtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKgInicisPGtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKgInicisPGtestApplication.class, args);
+    }
+
+}
